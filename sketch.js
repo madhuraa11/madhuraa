7e41b9c600dfcbb6c8824fc58f409a578ef40dc4
madhuraa11 <mbalapure1075@gmail.com>
2024-08-02 19:57:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,23 +1,42 @@
 function setup() {
-  cook("Chai", ["tea leaves", "water", "milk", "spices"]);
+  createCanvas(400, 400);
+  background(0,0,0);
+
+  let seedX = 200;
+  let seedY = 400;
+  let step = 30;
+  maxGenerations=7;
+  
+  
+  stroke(255);
+  strokeWeight(5);
+  branch(seedX, seedY, step,maxGenerations);
 }
 
-// taking name of the dish and list of ingredients
-function cook(whatToCook = "nothing", ingredients = []) {
-  console.log("Now cooking " + whatToCook);
+function branch(seedX, seedY, step, maxGenerations) {
+   maxGenerations=maxGenerations-1;
+  if (maxGenerations > 0) {
+   
+    let whereToGrowX = seedX;
+    let whereToGrowY = seedY - step;
 
-  // check if ingredients are not empty
-  if (ingredients.length > 0) {
-    showRecipe(ingredients);
-  } else {
-    console.log("Missing all ingredients");
-  }
-}
+    let whereToGoLeftX = seedX - step;
+    let whereToGoLeftY = seedY - step - step;
 
-// take ingredients
-// and list the recipe for a dish
-function showRecipe(collection = []) {
-  for(let x=0; x<collection.length; x++) {
-    console.log("element number-" + x + ": " + collection[x]);
-  }
+    line(seedX, seedY, whereToGrowX, whereToGrowY);
+
+    let pointLeftX = whereToGrowX - step;
+    let pointLeftY = whereToGrowY - step;
+
+    line(whereToGrowX, whereToGrowY, pointLeftX, pointLeftY);
+    branch(pointLeftX,pointLeftY,step, maxGenerations);
+    
+
+    let pointRightX = whereToGrowX + step;
+    let pointRightY = whereToGrowY - step;
+
+    line(whereToGrowX, whereToGrowY, pointRightX, pointRightY);
+    branch(pointRightX,pointRightY,step, maxGenerations);
+    
+  }
 }
